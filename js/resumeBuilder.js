b8a9a705cddc93d55c4d55fd6e2141b60a4566bf
washbz250 <washbz250@gmail.com>
2015-02-12 11:08:30 -0500

    Added resumeBuilder.old, Changed resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,38 +1,21 @@
 //Zachary Washburn
 //resumeBuilder.js
 //Helps build the resume page
-var skills = ["leadership", "Web dev", "JS", "HTML"];
-//Object declaration
-var bio = {
-	"name" : "Zach",
-	"age" : 32,
-	"skills" : skills
+
+var education = {
+	"schools": [
+	{
+		"name" : "Eckerd Collage",
+		"city" : "Florida, US",
+		"degree" : "BA",
+		"major" : ["Compsci", "French"]
+	},
+	{
+		"name" : "Washburn University",
+		"city" : "Nebraska, US",
+		"degree" : "Masters",
+		"major" : ["CompSci"]
+	}
+	]	
 };
 
-//$("#main").append(bio.name);
-// Add properties
-bio.city = "Mountain View";
-bio.email = "washbz250@gmail.com";
-
-// object.property = "Value"
-
-bio["email"] = "newemail@web.com";
-
-//object[property] = "Value";
-
-var work = {};
-
-work.position = "Lead Programmer";
-work.employer = "Myself";
-work.yearsWorked = "12";
-work.cityBusiness = "Coopersville";
-
-
-var education = {};
-
-education["lastSchool"] = "Coopersville High School";
-education["yearsAttended"] = "12";
-education["schoolCity"] = "Coopersville";
-
-$("#main").append(work.position);
-$("#main").append(education.lastSchool);
